3207bded3e8190ba63869746825040f3c7d5ec66
wxy1224 <xw288@cornell.edu>
2017-10-26 16:39:56 -0700

    pitch plot

--- librosa_demo.py
+++ librosa_demo.py
@@ -3,6 +3,7 @@ import matplotlib
 import numpy as np
 import librosa
 import time
+import matplotlib.pylab as plt
 
 debug = False
 
@@ -11,7 +12,7 @@ def main():
 	audio_file = 'data/Audio/LizNelson_Rainfall/' + audio_name + '.wav'
 	result_file = 'data/' + audio_name + '.txt'
 	raw_pitch, processed_pitch = baseline_tracking(audio_file, result_file)
-	pitch_plot() # pass in input as needed
+	pitch_plot(raw_pitch) # pass in input as needed
 
 def baseline_tracking(audio_file, result_file=None):
 	start = time.clock()
@@ -50,13 +51,14 @@ def baseline_tracking(audio_file, result_file=None):
 			ret_pitch += pitch_t,
 			if result_file:
 				for pitch in pitch_t:
-					file.write(str(t)+ ',' + str(pitch)+'\n')
+					#file.write(str(t)+ ',' + str(pitch)+'\n')
+					a = 1
 			else:
 				print(t)
 				print(pitch_t)
 				print()
-	print("Saved in " + result_file)
-	file.close()
+	# print("Saved in " + result_file)
+	# file.close()
 
 	print("Stat:")
 	print('{:f}s for piptrack'.format(time.clock()-start))
@@ -70,13 +72,53 @@ def baseline_tracking(audio_file, result_file=None):
 
 	return pitches, ret_pitch
 
+def plot(vector, name, xlabel=None, ylabel=None):
+    plt.figure()
+    plt.plot(vector)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot()
+    plt.savefig('pitch_plots/'+name)
+
+def smooth(x,window_len=11,window='hanning'):
+        if window_len<3:
+                return x
+        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+                raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+        s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        if window == 'flat': #moving average
+                w=np.ones(window_len,'d')
+        else:
+                w=eval('np.'+window+'(window_len)')
+        y=np.convolve(w/w.sum(),s,mode='same')
+        return y[window_len:-window_len+1]
+
+def extract_max_plot(pitches, shape):
+    new_pitches = []
+    for i in range(0, shape[1]):
+        new_pitches.append(np.max(pitches[:,i]))
+    return new_pitches
+
 def pitch_plot(pitches):
-	pass
 	# Xiaoyan: please modify this function
 	# x, y = [], []
 	# for i, notes_grp in enumerate(pitches):
 	# 	x.extend([i] * len(notes_grp))
 	# 	y.extend(notes_grp)
 	# plt.plot(x, y, label='time step (x) vs frequency (y)')
+	shape = np.shape(pitches)
+
+	pitches = extract_max_plot(pitches, shape)
+
+	pitches1 = smooth(pitches,window_len=10)
+	pitches2 = smooth(pitches,window_len=20)
+	pitches3 = smooth(pitches,window_len=30)
+	pitches4 = smooth(pitches,window_len=40)
+
+	plot(pitches1, 'pitches1')
+	plot(pitches2, 'pitches2')
+	plot(pitches3, 'pitches3')
+	plot(pitches4, 'pitches4')
+	plot(pitches, 'pitches')
 
 main()
