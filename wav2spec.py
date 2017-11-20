67d68691d7998b0834f69f79c6665111cbc0346c
unknown <liubb16@connect.hku.hk>
2017-11-19 16:05:34 -0800

    use librosa

--- wav2spec.py
+++ wav2spec.py
@@ -5,32 +5,44 @@ of frequencies in a sound.  Horizontal axis represents time, Vertical axis
 represents frequency, and color represents amplitude.
 """
 
-
 import os
-import wave
+import numpy as np
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
 
-import pylab
+# Generate time slices of spectrogram, which will be used for CNN training
+def wav2spec_data(audioName, fext):
+    sr = 44100 # sampling rate
+    y, sr = librosa.load(audioName+'.'+fext, sr=sr)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, fmax=8000, n_mels=256)
+    spec = librosa.power_to_db(S, ref=np.max)
 
+    for i in range(spec.shape[1]):
+        plt.figure(figsize=(1, 4)) # 100 * 400 pixels
+        plt.subplot(111)
+        librosa.display.specshow(spec[:, i:i+1], sr=sr)
+        plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+        plt.axis('off')
+        plt.savefig('spec_{:s}_{:d}.png'.format(audioName, i))
+        plt.close()
 
-def graph_spectrogram(wav_file):
-    sound_info, frame_rate = get_wav_info(wav_file)
-    pylab.figure(num=None, figsize=(19, 12))
-    pylab.subplot(111)
-    pylab.title('spectrogram of %r' % wav_file)
-    pylab.specgram(sound_info, Fs=frame_rate)
-    pylab.savefig('spectrogram_1_200th.png')
+# Generate a spectrogram for the entire audio for demo
+def wav2spec_demo(audioName, fext):
+    sr = 44100
+    y, sr = librosa.load(fname, sr=sr)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, fmax=8000, n_mels=256, y_axis='mel', x_axis="time")
+    spec = librosa.power_to_db(S, ref=np.max)
 
-
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    frames = frames[:int(len(frames)/50)]
-    sound_info = pylab.fromstring(frames, 'Int16')
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
-
+    plt.figure(figsize=(20, 4)) # 2000 * 400 pixels
+    plt.subplot(111)
+    librosa.display.specshow(spec[:, i:i+1], sr=sr)
+    # display the legend: color vs db
+    plt.colorbar(format='%+2.0f dB')
+    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+    plt.savefig('spec_' + audioName + '.png')
+    plt.close()
 
 if __name__ == '__main__':
-    wav_file = 'LizNelson_Rainfall_MIX.wav'
-    graph_spectrogram(wav_file)
+    audioName, file_ext = "Phoenix_ScotchMorris_MIX", 'wav'
+    wav2spec_data(audioName, file_ext)
