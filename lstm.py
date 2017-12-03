483402dd713245be289f4354b749f97a3e35f99c
ClaraBing <liubb16@connect.hku.hk>
2017-12-03 07:19:41 +0000

    try lstm for part 2

---
+++ lstm.py
@@ -0,0 +1,53 @@
+# Source: http://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader 
+from model_lstm import *
+
+model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+# See what the scores are before training
+# Note that element i,j of the output is the score for tag j for word i.
+inputs = prepare_sequence(training_data[0][0], word_to_ix)
+tag_scores = model(inputs)
+print(tag_scores)
+
+for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+    for sentence, tags in training_data:
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Also, we need to clear out the hidden state of the LSTM,
+        # detaching it from its history on the last instance.
+        model.hidden = model.init_hidden()
+
+        # Step 2. Get our inputs ready for the network, that is, turn them into
+        # Variables of word indices.
+        sentence_in = prepare_sequence(sentence, word_to_ix)
+        targets = prepare_sequence(tags, tag_to_ix)
+
+        # Step 3. Run our forward pass.
+        tag_scores = model(sentence_in)
+
+        # Step 4. Compute the loss, gradients, and update the parameters by
+        #  calling optimizer.step()
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+# See what the scores are after training
+inputs = prepare_sequence(training_data[0][0], word_to_ix)
+tag_scores = model(inputs)
+# The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
+#  for word i. The predicted tag is the maximum scoring tag.
+# Here, we can see the predicted sequence below is 0 1 2 0 1
+# since 0 is index of the maximum value of row 1,
+# 1 is the index of maximum value of row 2, etc.
+# Which is DET NOUN VERB DET NOUN, the correct sequence!
+print(tag_scores)
