d092d008c41dddecea0aa772524007cbe1fb7d85
root <root@instance-1.c.cs229-musictranscription.internal>
2017-12-04 00:50:31 +0000

    update lstm

--- train_lstm.py
+++ train_lstm.py
@@ -11,13 +11,16 @@ from util import *
 
 torch.manual_seed(1)
 
+n_features = 109
 n_classes = 109
 n_layers = 2
 n_epochs = 5
 n_batch = 9
 batch_size = 32
 
-model = nn.LSTM(n_features, n_classes, n_layers)
+model =  nn.LSTM(109,1024,2)
+model.cuda()
+
 optimizer = optim.Adam(model.parameters())
 
 kwargs = {}
@@ -27,13 +30,17 @@ annotations_train = '/root/MedleyDB_selected/Annotations/Melody_Annotations/MELO
 train_set = LSTMDataSet(annotations_train, '/root/CS229-MusicTranscription/dataset/val_lstm_input.npy')
 train_loader = DataLoader(train_set, batch_size=1, shuffle=True)
 
+print("train loader",len(train_loader.dataset))
+
 model.train()
 errors = []
 for epoch in range(n_epochs):
     training_loss = 0
     for idx, dictionary in enumerate(train_loader):
+        print("dic", dictionary['input'])
         batch_start = time()
         data, target = dictionary['input'], dictionary['freq_vec']
+        print("data, target",data, target)
         data, target = Variable(data).type(torch.FloatTensor), Variable(target).type(torch.FloatTensor)
         data, target = data.cuda(), target.cuda()
         
@@ -52,7 +59,11 @@ for epoch in range(n_epochs):
         optimizer.zero_grad()
         err.backward()
         optimizer.step()
+        if idx % 10 == 0:
+            print('Train Epoch: epoch {} iter {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime per batch: {:.6f}s'.format(epoch, idx, idx*len(data), len(train_loader.dataset), 100.*idx/len(train_loader.dataset), loss.data[0], time()-batch_start))
+            sys.stdout.flush()
     torch.save(model, 'lstm2_epoch{:d}.pt'.format(idx+1))
-    errors.append(training_loss/len(Xtrain_lst))
+    #errors.append(training_loss/len(Xtrain_lst))
+    
 training_loss = training_loss/len(Xtrain_lst)
 print("training loss:" +str(training_loss))
