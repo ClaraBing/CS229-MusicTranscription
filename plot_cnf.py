da9cfdc22914d863ca69f0a57417f967fef14469
ClaraBing <liubb16@connect.hku.hk>
2017-12-16 20:35:05 +0000

    clean up repo

--- plot_cnf.py
+++ plot_cnf.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -5,7 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-import itertools
+# our code
+from PitchEstimationDataSet import PitchEstimationDataSet
+import util
 
 def plot_confusion_matrix(cm, classes=[],
                           normalize=False,
@@ -21,31 +24,40 @@ def plot_confusion_matrix(cm, classes=[],
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
-    # ax=plt.gca() #get the current axes
-    # PCM=ax.get_children()[2] #get the mappable, the 1st and the 2nd are the x and y axes
-    # plt.colorbar(PCM, ax=ax) 
     plt.colorbar()
-#    tick_marks = np.arange(len(classes))
-#    plt.xticks(tick_marks, classes, rotation=45)
-#    plt.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
-    # for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    #     plt.text(j, i, format(cm[i, j], fmt),
-    #              horizontalalignment="center",
-    #              color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    plt.savefig('cf_mtrx_cropped.png')
+    plt.savefig('cf_mtrx.png')
 
-# y_true = [2, 0, 2, 2, 0, 1]
-# y_pred = [0, 0, 2, 2, 0, 2]
-# cnf = confusion_matrix(y_true, y_pred)
-# plot_confusion_matrix(cnf)
 
-cnf_matrix = np.load('cnf_matrix.npy')
-print(cnf_matrix.shape)
-plot_confusion_matrix(cnf_matrix[20:80, 0:80], normalize = True)
+val_result = np.load('dataset/val_result_mtrx.npy')
+annotations_val = '/root/MedleyDB_selected/Annotations/Melody_Annotations/MELODY1/val/'
+val_set = PitchEstimationDataSet(annotations_val, '/root/data/val/', sr_ratio = 2, audio_type='MIX')
+
+val_pitches = []
+for i in range(val_result.shape[0]):
+	pitch_frame = val_result[i]
+	max = -1
+	max_pitch = 0
+	for j in range(val_result.shape[1]):
+		if (pitch_frame[j][0]>max):
+			max = pitch_frame[j][0]
+			max_pitch = pitch_frame[j][1]
+	val_pitches.append(max_pitch)
+val_pitches = np.asarray(val_pitches)
+
+val_labels = []
+for pitches in val_set.pitches:
+	val_labels += pitches
+val_labels = np.asarray(val_labels)
+
+sampled_val_pitches = util.subsample(val_pitches, val_labels)
+
+labels = range(109)
+cnf_matrix = confusion_matrix(val_labels, sampled_val_pitches, labels = labels)
+plot_confusion_matrix(cnf_matrix, title = 'cnf matrix')
